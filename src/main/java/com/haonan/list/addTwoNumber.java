fc9d6aae1664807e2eb85b48f1f0402b1a347311
haonanren <haonanren@sohu-inc.com>
2019-05-31 15:08:19 +0800

    add leetcode 2

---
+++ src/main/java/com/haonan/list/addTwoNumber.java
@@ -0,0 +1,56 @@
+package com.haonan.list;
+
+/**
+ * @Author rhaonan
+ * @Data 2019/5/31
+ * @Description 两数相加
+ * leetcode:2
+ */
+public class addTwoNumber {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode p = l1, q = l2, curr = head;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum%10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        System.out.println(result);
+    }
+}
