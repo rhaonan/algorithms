fe7717f513a6636eec74feef66651aec6817f68f
haonanren <haonanren@sohu-inc.com>
2019-08-02 17:02:29 +0800

    update

--- src/main/java/com/haonan/leetcode/Code3.java
+++ src/main/java/com/haonan/leetcode/Code3.java
@@ -2,8 +2,7 @@ package com.haonan.leetcode;
 
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author rhaonan
@@ -22,7 +21,7 @@ public class Code3 {
         List<Character> charList = new ArrayList<>();
         for (int i = 0; i < len; i++) {
             if (charList.contains(s.charAt(i))) {
-                max = charList.size() > max ? charList.size() : max;
+                max = Math.max(charList.size(), max);
                 deleteBeforeChar(charList, s.charAt(i));
                 charList.add(s.charAt(i));
             } else {
@@ -30,7 +29,7 @@ public class Code3 {
             }
         }
 
-        max = charList.size() > max ? charList.size() : max;
+        max = Math.max(charList.size(), max);
         return max;
     }
 
@@ -41,8 +40,40 @@ public class Code3 {
         }
     }
 
+    public static int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstring3(String s) {
+        int ant = 0;
+        int n= s.length();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = map.get(s.charAt(j));
+            }
+            ant = Math.max(ant, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ant;
+    }
+
     public static void main(String[] args) {
         String test = "vdadfe";
-        System.out.println(lengthOfLongestSubstring(test));
+        System.out.println(lengthOfLongestSubstring2(test));
+        System.out.println(lengthOfLongestSubstring3(test));
     }
 }
