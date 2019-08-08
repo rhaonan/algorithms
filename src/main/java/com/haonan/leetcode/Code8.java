caaadccf7b1b1681a9b9a8b08358d16538fb97a4
haonanren <haonanren@sohu-inc.com>
2019-08-08 17:25:54 +0800

    add code 8

---
+++ src/main/java/com/haonan/leetcode/Code8.java
@@ -0,0 +1,17 @@
+package com.haonan.leetcode;
+
+/**
+ * @Author rhaonan
+ * @Data 2019/8/8
+ * @Description
+ */
+public class Code8 {
+
+    public static int myAtoi(String str) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
